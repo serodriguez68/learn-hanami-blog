3a451bc6c0bc40f5912174bc0c419db2488f8579
Sergio Rodriguez <se.rodriguez68@gmail.com>
2020-06-14 20:19:11 +1000

    Extends Dry::Validation::Result to return a Failure[:validation,self] monad

--- lib/learn_hanami_blog/validation/contract.rb
+++ lib/learn_hanami_blog/validation/contract.rb
@@ -12,3 +12,16 @@ module LearnHanamiBlog
     end
   end
 end
+
+Dry::Validation.load_extensions(:monads)
+
+module DryValidationResultMonadExtensions
+  # Overrides the default behaviour so that all validation failures
+  # have the :validation failure code by default.
+  # If a more specific failure code is needed, handle it in the object
+  # that calls the contract.
+  def to_monad
+    success? ? super : Failure[:validation, self]
+  end
+end
+Dry::Validation::Result.prepend DryValidationResultMonadExtensions
